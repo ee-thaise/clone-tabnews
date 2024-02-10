910607b62a5a3ac25f1cc3d3496f6f9f180fe259
ee_thaise <44025356+ee-thaise@users.noreply.github.com>
2024-02-10 18:47:00 +0000

    Adicionando emojis apaixonados

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>DIA 25/05/2025 você está convidado para o meu casamento, apareça com roupa de noivo e tome banho</h1>
+    return <h1>DIA 25/05/2025 você está convidado para o meu casamento, apareça com roupa de noivo e tome banho 😍🥰</h1>
 }
 
 export default Home;
